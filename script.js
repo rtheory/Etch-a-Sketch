ea29ba0cbf07057bbf0532d78f26d96174a3424e
rTheory <royer.travis@gmail.com>
2019-08-29 01:58:27 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const gridContainer = document.getElementById("gridContainer");
+let gridSize = 16;
+
+function generateGrid(){
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+    // Generate array of gridSizexgridSize square divs
+    for (let row = 0; row < gridSize; row++){
+        for (let col = 0; col < gridSize; col++){
+            let div = document.createElement("div");
+            div.className = "square";
+            div.textContent = '';
+            gridContainer.appendChild(div);
+        }
+    }
+
+    // Add event listener for hover
+    let squares = document.querySelectorAll("div.square");
+    squares.forEach(square => {
+        square.addEventListener('mouseover', function(e){
+            console.log(e);
+            e.target.style.backgroundColor = 'red';
+        })
+    });
+}
+
+function clearGrid(){
+    let gridContainerChild = gridContainer.lastElementChild;
+    while (gridContainerChild){
+        gridContainer.removeChild(gridContainerChild);
+        gridContainerChild = gridContainer.lastElementChild;
+    }
+}
+
+// Add event listener for reset button
+let btnReset = document.querySelector("button#reset");
+btnReset.addEventListener('click', function(e){
+    gridSize = prompt('How large would you like the grid (number of squares)?');
+    clearGrid();
+    generateGrid();
+})
+
+generateGrid();
